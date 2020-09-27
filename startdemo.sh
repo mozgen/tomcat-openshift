843029595b6f32be96cb6718e7ec387e0692f815
Jean-Frederic Clere <jfclere@gmail.com>
2020-09-27 13:51:17 +0200

    Add loadbalancer.yaml and adjust startdemo.sh

--- startdemo.sh
+++ startdemo.sh
@@ -2,7 +2,7 @@ kubectl create namespace tomcat-demo
 kubectl config set-context --current --namespace=tomcat-demo
 kubectl create -f kube-tomcat-demo.yaml
 kubectl create -f service.yaml
-kubectl expose deployment tomcat-demo --type=LoadBalancer --name=tomcat-balancer
+kubectl create -f loadbalancer.yaml
 kubectl get services
 kubectl get deployment
 kubectl scale --replicas=2 deployment/tomcat-demo
